07afc06426e39c5f51e9fb3d8627d9fe9e1bc567
James <jclavin2@users.noreply.github.com>
2021-03-07 09:28:12 -0500

    6 tuned for LAN and alerts

--- profile.py
+++ profile.py
@@ -9,8 +9,8 @@ pc = portal.Context()
 #disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:CENTOS8-64-STD"; #centos 8.
 #disk_image = "urn:publicid:IDN+emulab.net+image+ByzantineGridSec:Prime6.node0" #centos 8 with prime
 #disk_image = "urn:publicid:IDN+apt.emulab.net+image+byzantinegridsec-PG0:Prime12.node1" #centos 8 with prime configured for 6 nodes, 1 PLC/RTU, 1 HMI
-disk_image = "urn:publicid:IDN+apt.emulab.net+image+byzantinegridsec-PG0:Prime14.node1" #centos 8 with prime configured for 6 nodes, 1 PLC/RTU, 1 HMI with 512 bit keys.
-
+#disk_image = "urn:publicid:IDN+apt.emulab.net+image+byzantinegridsec-PG0:Prime14.node1" #centos 8 with prime configured for 6 nodes, 1 PLC/RTU, 1 HMI with 512 bit keys.
+disk_image = "urn:publicid:IDN+apt.emulab.net+image+byzantinegridsec-PG0:Prime15.node1" #cenots 8 with prime configured for 6 nodes tuned for LAN and with alerts. 
 
 
 # Create a Request object to start building the RSpec.
